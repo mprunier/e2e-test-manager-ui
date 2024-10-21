608b45a6ef2e213c94c297d9969dac703c07a270
Maxime PRUNIER <maxime.prunier@itsf.io>
2024-10-21 21:26:34 +0200

    update to 1.1.O

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
     return (
         <footer className="flex w-full items-center justify-center">
             <div className="p-4">
-                <span className="text-center text-sm text-gray-400">© 2024 By Maxime Prunier - Version 1.0.1</span>
+                <span className="text-center text-sm text-gray-400">© 2024 By Maxime Prunier - Version 1.1.0</span>
             </div>
         </footer>
     );
